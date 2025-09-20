d66fc84b1705ad380ee2c7d50fa28f22e3478bcd
Teo Jia Cheng <jcteo.2018@scis.smu.edu.sg>
2025-09-20 17:46:39 +0800

    Comment routerOutlet in src\app\app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+// import { RouterOutlet } from '@angular/router';
 import { TaskListComponent } from './components/task-list/task-list.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, TaskListComponent],
+  // imports: [RouterOutlet, TaskListComponent],
+  imports: [TaskListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
   standalone: true
